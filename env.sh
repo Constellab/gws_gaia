f15463b2e6357f2a754871c79c9c8fa27b7c3dad
Djomangan Adama OUATTARA <douattara@gencovery.com>
2020-08-22 10:54:05 +0200

    Use new architecture with exepriment

--- env.sh
+++ env.sh
@@ -1,14 +1,14 @@
 
-python -m pip install --upgrade pip
-python -m pip install virtualenv
-python -m virtualenv .venv --python=python3
+python3 -m pip install --upgrade pip
+python3 -m pip install virtualenv
+python3 -m virtualenv .venv --python=python3
 
 # activate vitual env
 . ./.venv/bin/activate
 #. ./.venv/Scripts/activate
 
 # prism requirement file
-python -m pip install -r ../gws-py/requirements.txt
+python3 -m pip install -r ../gws-py/requirements.txt
 
 # current requirement file
-python -m pip install -r requirements.txt
+python3 -m pip install -r requirements.txt

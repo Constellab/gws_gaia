18fbbbf62e21fbac3d3440444269917476d3bea8
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-06-06 18:57:27 +0000

    [refactor] code refactoring

--- dep/install.sh
+++ dep/install.sh
@@ -4,5 +4,25 @@
 # About us: https://gencovery.com
 
 
-# Installation script executed on server first startup
+echo "Building dlib ..."
 
+build_dir="/app/lab/.gws/externs/dlib-cpp/build"
+ready_file="$build_dir/READY"
+if [ ! -f "$ready_file" ]; then
+    if [ ! -d "$build_dir" ]; then
+        mkdir -p $build_dir
+    fi
+    
+    cd $build_dir
+    cmake -DUSE_AVX_INSTRUCTIONS=ON -DBUILD_SHARED_LIBS=1 ..
+    cmake --build . --config Release
+    make
+    touch $ready_file
+fi
+
+echo "Build done!"
+echo "Installing dlib ..."
+
+cd $build_dir
+make install
+ldconfig

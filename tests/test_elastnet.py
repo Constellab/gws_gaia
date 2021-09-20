aa17d5e030d73f06e792006d20d560ddf8c5998e
root <waboujaoude@gencovery.com>
2021-09-20 10:00:53 +0000

    new version

--- tests/test_elastnet.py
+++ tests/test_elastnet.py
@@ -1,56 +1,107 @@
 import os
 import asyncio
-from unittest import BaseTestCase
 
 from gws_gaia import Dataset, DatasetLoader
 from gws_gaia import ElasticNetTrainer, ElasticNetPredictor, ElasticNetTester
 from gws_core import Settings, GTest, BaseTestCase, TaskTester
 
-class TestTrainer(IsolatedAsyncioTestCase):
+class TestTrainer(BaseTestCase):
 
-        
     async def test_process(self):
         GTest.print("Elastic Net")
         settings = Settings.retrieve()
         test_dir = settings.get_variable("gws_gaia:testdata_dir")
 
-        p0 = DatasetLoader()
-        p1 = ElasticNetTrainer()
-        p2 = ElasticNetPredictor()
-        p3 = ElasticNetTester()
-        
-        proto = Protocol(
-            processes = {
-                'p0' : p0,
-                'p1' : p1,
-                'p2' : p2,
-                'p3' : p3                
-            },
-            connectors = [
-        p0>>'dataset' | p1<<'dataset',
-        p0>>'dataset' | p2<<'dataset',
-        p1>>'result' | p2<<'learned_model',
-        p0>>'dataset' | p3<<'dataset',
-        p1>>'result' | p3<<'learned_model'
-            ]
+        #import data
+        dataset = Dataset.import_from_path(os.path.join(
+            test_dir, "./diabetes.csv"), 
+            delimiter=",", 
+            header=0, 
+            targets=['target']
         )
 
-        p0.set_param("delimiter", ",")
-        p0.set_param("header", 0)
-        p0.set_param('targets', ['target'])
-        p0.set_param("file_path", os.path.join(test_dir, "./diabetes.csv"))
-        p1.set_param('alpha', 1) 
+        # run trainer
+        tester = TaskTester(
+            params = {'alpha': 1},
+            inputs = {'dataset': dataset},
+            task_type = ElasticNetTrainer
+        )
+        outputs = await tester.run()
+        trainer_result = outputs['result']
 
-        experiment: Experiment = Experiment(
-            protocol=proto, study=GTest.study, user=GTest.user)
-        experiment.save()
-        experiment = await ExperimentService.run_experiment(
-            experiment=experiment, user=GTest.user)
+        # run predictior
+        tester = TaskTester(
+            params = {},
+            inputs = {
+                'dataset': dataset, 
+                'learned_model': trainer_result
+            },
+            task_type = ElasticNetPredictor
+        )
+        outputs = await tester.run()
+        predictor_result = outputs['result']
 
-        r1 = p1.output['result']
-        r2 = p2.output['result']
-        r3 = p3.output['result']
+        # run tester
+        tester = TaskTester(
+            params = {},
+            inputs = {
+                'dataset': dataset, 
+                'learned_model': trainer_result
+            },
+            task_type = ElasticNetTester
+        )
+        outputs = await tester.run()
+        tester_result = outputs['result']
+
+        print(trainer_result)
+        print(tester_result)
+        print(predictor_result)
+
+# class TestTrainer(IsolatedAsyncioTestCase):
+
         
-        # print(r1)
-        # print(r2)
-        print(r3.tuple)
+#     async def test_process(self):
+#         GTest.print("Elastic Net")
+#         settings = Settings.retrieve()
+#         test_dir = settings.get_variable("gws_gaia:testdata_dir")
+
+#         p0 = DatasetLoader()
+#         p1 = ElasticNetTrainer()
+#         p2 = ElasticNetPredictor()
+#         p3 = ElasticNetTester()
+        
+#         proto = Protocol(
+#             processes = {
+#                 'p0' : p0,
+#                 'p1' : p1,
+#                 'p2' : p2,
+#                 'p3' : p3                
+#             },
+#             connectors = [
+#         p0>>'dataset' | p1<<'dataset',
+#         p0>>'dataset' | p2<<'dataset',
+#         p1>>'result' | p2<<'learned_model',
+#         p0>>'dataset' | p3<<'dataset',
+#         p1>>'result' | p3<<'learned_model'
+#             ]
+#         )
+
+#         p0.set_param("delimiter", ",")
+#         p0.set_param("header", 0)
+#         p0.set_param('targets', ['target'])
+#         p0.set_param("file_path", os.path.join(test_dir, "./diabetes.csv"))
+#         p1.set_param('alpha', 1) 
+
+#         experiment: Experiment = Experiment(
+#             protocol=proto, study=GTest.study, user=GTest.user)
+#         experiment.save()
+#         experiment = await ExperimentService.run_experiment(
+#             experiment=experiment, user=GTest.user)
+
+#         r1 = p1.output['result']
+#         r2 = p2.output['result']
+#         r3 = p3.output['result']
+        
+#         # print(r1)
+#         # print(r2)
+#         print(r3.tuple)

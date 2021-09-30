4de15a5da31d8cec4c0913e48184b7504b11c25f
Djomangan Adama OUATTARA <douattara@gencovery.com>
2021-09-30 19:18:30 +0000

    (refactor)[test] remove commented lines

--- tests/test_regularizationlayers.py
+++ tests/test_regularizationlayers.py
@@ -31,34 +31,3 @@ class TestTrainer(BaseTestCase):
         result = outputs['result']
 
         print(result)
-
-
-# class TestTrainer(BaseTestCase):
-
-        
-#     async def test_process(self):
-#         self.print("Dropout layer")
-#         p1 = InputConverter()
-#         p2 = Dropout()
-
-#         proto = Protocol(
-#             processes = {
-#                 'p1' : p1,
-#                 'p2' : p2
-#             },
-#             connectors = [
-#                 p1>>'result' | p2<<'tensor'
-#             ]
-#         )
-
-#         p1.set_param('input_shape', [None, 3, 3])    
-#         p2.set_param('rate', 0.5)    
-
-#         experiment: Experiment = Experiment(
-#             protocol=proto, study=GTest.study, user=GTest.user)
-#         experiment.save()
-#         experiment = await ExperimentService.run_experiment(
-#             experiment=experiment, user=GTest.user)                
-        
-#         r = p2.output['result']
-#         print(r)
--- tests/test_reshapinglayers.py
+++ tests/test_reshapinglayers.py
@@ -30,34 +30,3 @@ class TestTrainer(BaseTestCase):
         result = outputs['result']
 
         print(result)
-
-
-
-# class TestTrainer(BaseTestCase):
-
-        
-#     async def test_process(self):
-#         self.print("Flatten layer")
-#         p1 = InputConverter()
-#         p2 = Flatten()
-
-#         proto = Protocol(
-#             processes = {
-#                 'p1' : p1,
-#                 'p2' : p2
-#             },
-#             connectors = [
-#                 p1>>'result' | p2<<'tensor'
-#             ]
-#         )
-
-#         p1.set_param('input_shape', [3, 3, 3])    
-
-#         experiment: Experiment = Experiment(
-#             protocol=proto, study=GTest.study, user=GTest.user)
-#         experiment.save()
-#         experiment = await ExperimentService.run_experiment(
-#             experiment=experiment, user=GTest.user)    
-        
-#         r = p2.output['result']
-#         print(r)

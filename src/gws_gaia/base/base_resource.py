2e06d90873367311db7786037ff646f016367b76
Djomangan OUATTARA <douattara@gencovery.com>
2021-12-20 21:13:54 +0000

    (fix) remove mandatory parameters from constructor

--- src/gws_gaia/base/base_resource.py
+++ src/gws_gaia/base/base_resource.py
@@ -1,21 +1,22 @@
 # LICENSE
-# This software is the exclusive property of Gencovery SAS. 
+# This software is the exclusive property of Gencovery SAS.
 # The use and distribution of this software is prohibited without the prior consent of Gencovery SAS.
 # About us: https://gencovery.com
 
 from typing import Any
-from gws_core import (Resource, resource_decorator, RField)
 
+from gws_core import Resource, RField, resource_decorator
 
-#==============================================================================
-#==============================================================================
+# ==============================================================================
+# ==============================================================================
+
 
 @resource_decorator("BaseResource", hide=True)
 class BaseResource(Resource):
     result: Any = RField(default_value=None)
 
-    def __init__(self, result=None, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, result=None):
+        super().__init__()
         if result is not None:
             self.result = result
 

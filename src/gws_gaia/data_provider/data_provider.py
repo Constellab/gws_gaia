8478fa6f713df21f42cde2e21fb084570f3883e2
Benjamin Maisonneuve <bmaisonneuve@gencovery.com>
2026-01-05 09:30:57 +0000

    (feat) use new variable format in settings.json

--- src/gws_gaia/data_provider/data_provider.py
+++ src/gws_gaia/data_provider/data_provider.py
@@ -1,15 +1,12 @@
-
-
 import os
 
 from gws_core import File, Settings, Table, TableImporter
 
 
-class DataProvider():
-
+class DataProvider:
     @classmethod
     def _get_test_data_dir(cls) -> str:
-        return Settings.get_instance().get_variable("gws_gaia:testdata_dir")
+        return Settings.get_instance().get_variable("gws_gaia", "testdata_dir")
 
     @classmethod
     def get_test_data_path(cls, path: str) -> str:
@@ -17,45 +14,55 @@ class DataProvider():
 
     @classmethod
     def get_distance_table_file(cls) -> File:
-        return File(cls.get_test_data_path('distance_matrix.csv'))
+        return File(cls.get_test_data_path("distance_matrix.csv"))
 
     @classmethod
     def get_digits_file(cls) -> File:
-        return File(cls.get_test_data_path('digits.csv'))
+        return File(cls.get_test_data_path("digits.csv"))
 
     @classmethod
     def get_diabetes_file(cls) -> File:
-        return File(cls.get_test_data_path('diabetes.csv'))
+        return File(cls.get_test_data_path("diabetes.csv"))
 
     @classmethod
     def get_digits_table(cls, header=0, targets=[]) -> Table:
-        return TableImporter.call(cls.get_digits_file(), {
-            "delimiter": ",",
-            "header": header,
-            # "targets": targets,
-            "metadata_columns": [{
-                "column": k,
-                "keep_in_table": True,
-            } for k in targets]
-        })
+        return TableImporter.call(
+            cls.get_digits_file(),
+            {
+                "delimiter": ",",
+                "header": header,
+                # "targets": targets,
+                "metadata_columns": [
+                    {
+                        "column": k,
+                        "keep_in_table": True,
+                    }
+                    for k in targets
+                ],
+            },
+        )
 
     @classmethod
-    def get_diabetes_table(cls, header=0, targets=['target']) -> Table:
-        return TableImporter.call(cls.get_diabetes_file(), {
-            "delimiter": ",",
-            "header": header,
-            # "targets": targets,
-            "metadata_columns": [{
-                "column": k,
-                "keep_in_table": True,
-            } for k in targets]
-        })
+    def get_diabetes_table(cls, header=0, targets=["target"]) -> Table:
+        return TableImporter.call(
+            cls.get_diabetes_file(),
+            {
+                "delimiter": ",",
+                "header": header,
+                # "targets": targets,
+                "metadata_columns": [
+                    {
+                        "column": k,
+                        "keep_in_table": True,
+                    }
+                    for k in targets
+                ],
+            },
+        )
 
     @classmethod
     def get_distance_table(cls) -> Table:
-        return TableImporter.call(cls.get_distance_table_file(), {
-            "delimiter": "tab",
-            "header": 0,
-            "index_column": 0,
-            "metadata_columns": []
-        })
+        return TableImporter.call(
+            cls.get_distance_table_file(),
+            {"delimiter": "tab", "header": 0, "index_column": 0, "metadata_columns": []},
+        )
--- tests/gws_gaia_test_helper.py
+++ tests/gws_gaia_test_helper.py
@@ -1,15 +1,12 @@
-
-
 import os
 
 from gws_core import File, Settings, Table, TableImporter
 
 
-class GWSGaiaTestHelper():
-
+class GWSGaiaTestHelper:
     @classmethod
     def get_test_data_dir(cls) -> str:
-        return Settings.get_instance().get_variable("gws_gaia:testdata_dir")
+        return Settings.get_instance().get_variable("gws_gaia", "testdata_dir")
 
     @classmethod
     def get_test_data_path(cls, *path: str) -> str:
@@ -17,15 +14,21 @@ class GWSGaiaTestHelper():
 
     @classmethod
     def get_data_file(cls, index=1) -> File:
-        return File(cls.get_test_data_path('dataset'+str(index)+'.csv'))
+        return File(cls.get_test_data_path("dataset" + str(index) + ".csv"))
 
     @classmethod
     def get_table(cls, index=1, header=None, targets=[]) -> Table:
-        return TableImporter.call(cls.get_data_file(index=index), {
-            "delimiter": ",",
-            "header": header,
-            "metadata_columns": [{
-                "column": k,
-                "keep_in_table": True,
-            } for k in targets]
-        })
+        return TableImporter.call(
+            cls.get_data_file(index=index),
+            {
+                "delimiter": ",",
+                "header": header,
+                "metadata_columns": [
+                    {
+                        "column": k,
+                        "keep_in_table": True,
+                    }
+                    for k in targets
+                ],
+            },
+        )
